8ef9644265b9e8b0b9e70a6d90576232a5b272ea
Takashi <ygortn14@gmail.com>
2021-02-26 18:24:00 -0400

    Ultimo dia colocando o efeito de level up e deixando ela pra fazer deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,8 @@
 import '../styles/global.css'
-import {ChallengesContext, ChallengesProvider} from '../contexts/ChallengesContext'
-import { useState } from 'react'
-import { CountdownProvider } from '../contexts/CountdownContext'
 
 function MyApp({ Component, pageProps }) {
-  
-  
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
-      
+  return (  
+    <Component {...pageProps} />    
   )
 }
 
